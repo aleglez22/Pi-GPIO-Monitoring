817e9dc08058e8250cef1c726abfa104536169e4
Alejandro González <aleglez22@gmail.com>
2018-06-16 13:34:04 -0500

    Structure/data of the Table used by the controller

---
+++ gpio.sql
@@ -0,0 +1,75 @@
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (1, 2, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (2, 3, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (3, 4, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (4, 5, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (5, 6, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (6, 7, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (7, 8, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (8, 9, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (9, 10, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (10, 11, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (11, 12, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (12, 13, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (13, 16, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (14, 17, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (15, 18, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (16, 19, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (17, 20, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (18, 21, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (19, 22, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (20, 23, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (21, 24, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (22, 25, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (23, 26, 'Testing', 0, 0, 0, null);
+INSERT INTO public.gpioinfo(
+	id, gpionumber, description, enabled, state, count, date)
+	VALUES (24, 27, 'Testing', 0, 0, 0, null);
+
+
+
